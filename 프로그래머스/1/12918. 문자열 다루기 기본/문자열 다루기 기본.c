ab24ec890c89e61f1494872a3e082524dc8b76a9
Jung Dayeon <102217402+al1kite@users.noreply.github.com>
2023-10-22 20:03:32 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+bool solution(const char* s) {
+    bool answer = true;
+    int cnt = 0;
+    for(int i = 0 ; s[i] != '\0'; i++){
+        if(!isdigit(s[i]))
+            answer = false;
+        cnt++;
+    }
+    if(cnt != 4 && cnt != 6)
+        answer = false;
+    return answer;
+}
