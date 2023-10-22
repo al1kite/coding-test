97f03efc754506bc090c306532c7dfdfc8e726d0
Jung Dayeon <102217402+al1kite@users.noreply.github.com>
2023-10-22 19:58:09 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+    int cnt = 0; 
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < numbers.length; j++)
+            if(numbers[j] != i)
+                cnt++;
+        if(cnt == numbers.length)
+            answer += i;
+        cnt = 0;
+    }
+        return answer;
+    }
+}
